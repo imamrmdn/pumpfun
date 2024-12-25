c4ac1448ab47d2c426ca94fad3dc1bf978cc620d
imamrmdn <imamramadhan420@gmail.com>
2024-12-25 23:06:36 +0700

    update code

--- app/modules/list/index.tsx
+++ app/modules/list/index.tsx
@@ -1,11 +1,40 @@
+"use client"
+
 import { Stack, } from "@mui/material"
 import { PumpCoin } from "common/types"
 import CoinCard from "./item"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export default function List({ data }: { data: PumpCoin[] }) {
+export default function List() {
+  //
+  const [data, setData] = useState<PumpCoin[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://gmgn.ai/defi/quotation/v1/rank/sol/pump?limit=50&orderby=progress&direction=desc&pump=true"
+        );
+        console.log({ log: response?.data })
+        setData(response.data || []);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
+  //
   return (
     <Stack p={1.5} direction="row" flexWrap="wrap" gap={1.5}>
-      {data.map(t => (
+      {data?.map(t => (
         <CoinCard key={t.address} data={t} />
       ))}
     </Stack>
--- common/api/index.ts
+++ common/api/index.ts
@@ -1,18 +1,34 @@
-import { PumpCoin, PumpDetail } from "common/types"
+import { PumpCoin, PumpDetail } from "common/types";
+import axios from 'axios';
 
-export async function getPumpList(): Promise<PumpCoin[]>{
-  const res = await fetch("https://gmgn.ai/defi/quotation/v1/rank/sol/pump?limit=50&orderby=progress&direction=desc&pump=true", {cache: "no-store"})
-  const data = await res.json()
-  if(data.code === 0){
-    return data.data.rank
+
+export async function getPumpList(): Promise<PumpCoin[]> {
+  try {
+    const res = await axios.get(
+      "https://gmgn.ai/defi/quotation/v1/rank/sol/pump?limit=50&orderby=progress&direction=desc&pump=true",
+      {
+        headers: {
+          "Cache-Control": "no-store",
+        },
+      }
+    );
+    console.log({ res });
+    if (res.data.code === 0) {
+      return res.data.data.rank;
+    }
+    return [];
+  } catch (error) {
+    console.error("Error fetching pump list:", error);
+    return [];
   }
-  return []
 }
 
-export async function getPumpDetail(addr: string): Promise<PumpDetail>{
-  const res = await fetch(`https://gmgn.ai/defi/quotation/v1/tokens/sol/${addr}`)
-  const data = await res.json()
-  if(data.code === 0){
-    return data.data.token
+export async function getPumpDetail(addr: string): Promise<PumpDetail> {
+  const res = await fetch(
+    `https://gmgn.ai/defi/quotation/v1/tokens/sol/${addr}`
+  );
+  const data = await res.json();
+  if (data.code === 0) {
+    return data.data.token;
   }
 }
